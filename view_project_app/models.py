57a5fb39cfb285d59bdad98d193a5fd6b434a5c1
Ajaypidha <ajaypidha408@gmail.com>
2025-09-09 00:41:00 +0530

    aded new field in Book table

--- view_project_app/models.py
+++ view_project_app/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     author = models.CharField(max_length=50)
     published_year = models.IntegerField()
     address = models.CharField(max_length=200, default='')
-
+    grade = models.CharField(max_length=10, default='A')
     def __str__(self):
         return self.title
     
@@ -29,6 +29,7 @@ class AjaySchool(models.Model):
 class New_changes(models.Model):
     name = models.CharField(max_length=100)
     published_year = models.IntegerField()
+    address = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.title
